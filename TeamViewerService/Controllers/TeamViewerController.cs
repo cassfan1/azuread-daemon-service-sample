5afa527dd40e4d934361525b53b68ca277c0229f
Cass Fan <WFAN@beckman.com>
2020-07-07 11:39:56 +0800

    Add project files.

---
+++ TeamViewerService/Controllers/TeamViewerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamViewerService.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TeamViewerController : ControllerBase
+  {
+    [HttpGet]
+    [Authorize(Policy = "Policy")]
+    public string Get()
+    {
+      return "Successfully";
+    }
+  }
+}
---
+++ TeamViewerService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamViewerService.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    [Authorize(Policy = "Policy1")]
+    [HttpGet]
+    public string Get()
+    {
+      return "Successfully";
+    }
+  }
+}
